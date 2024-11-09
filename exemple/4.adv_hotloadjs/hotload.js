7be5012bd5f285e3bca1b3d9887bc4d6500d9468
Jonathan BAUER <jonathan@jbauer.fr>
2024-11-09 16:27:56 +0100

    ADDED: Exemple 3 and 4.
    Signed-off-by: Jonathan BAUER <contact@jbauer.fr>

---
+++ exemple/4.adv_hotloadjs/hotload.js
@@ -0,0 +1,44 @@
+/* !
+ * |==================================|
+ * |        Jo Windows Manager        |
+ * |==================================|
+ * |
+ * |     Ver : 1.0.0
+ * |    Date : 02/11/2024
+ * |      By : Jonathan BAUER (jbauer.fr)
+ * | Copyright 2024-2025 The JWM Author (Jonathan BAUER)
+ * | License : Apache 2.0 ()
+ * |
+ * |==================================
+ * This is the js file used for the exemple 4.
+*/
+
+var VarWithUniqueNameToRemoveWhenUnloaded = {t:"000", d:true}
+
+function JWM_JS_LOAD() {
+    alert("JavaScript file loaded!");
+    console.log("JavaScript file loaded!");
+}
+
+function JWM_JS_UNLOAD() {
+    alert("JavaScript file unloaded!");
+    console.log("JavaScript file unloaded!");
+
+    // Unload variable, object, table, etc...
+    VarWithUniqueNameToRemoveWhenUnloaded = null;
+}
+
+function JWM_JS_OPEN(obj_window, ...args) {
+    alert(`A new window (${obj_window.toString()}) using this file has been opened with arguments:`, args);
+    console.log(`A new window (${obj_window.toString()}) using this file has been opened with arguments:`, args);
+}
+
+function JWM_JS_UPDATE(obj_window, ...args) {
+    alert(`A window (${obj_window.toString()}) using this file has been updated with arguments:`, args);
+    console.log(`A window (${obj_window.toString()}) using this file has been updated with arguments:`, args);
+}
+
+function JWM_JS_CLOSE(obj_window, ...args) {
+    alert(`A window (${obj_window.toString()}) using this file has been closed with arguments:`, args);
+    console.log(`A window (${obj_window.toString()}) using this file has been closed with arguments:`, args);
+}
